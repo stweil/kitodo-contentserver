dd13b1ac164197086f4ad341f6448c7ce7d54a4f
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2012-02-28 21:16:35 +0000

    changed license header

--- intrandaContentServer/src/de/unigoettingen/sub/commons/contentlib/exceptions/CacheException.java
+++ intrandaContentServer/src/de/unigoettingen/sub/commons/contentlib/exceptions/CacheException.java
@@ -3,9 +3,13 @@
  * Visit the websites for more information. 
  * 		- http://gdz.sub.uni-goettingen.de 
  * 		- http://www.intranda.com 
+ * 		- http://www.digiverso.com
  * 
  * Copyright 2009, Center for Retrospective Digitization, Göttingen (GDZ),
- * intranda software.
+ * intranda software
+ * 
+ * This is the extended version updated by intranda
+ * Copyright 2012, intranda GmbH
  * 
  * Licensed under the Apache License, Version 2.0 (the “License�?);
  * you may not use this file except in compliance with the License.
